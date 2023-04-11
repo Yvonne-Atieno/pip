55df867e9e6bcbc3e1fb1c2144c37e8bb5b586ec
NjeriMarion <njeriwmarion@gmail.com>
2023-04-11 13:43:24 +0300

    first commit

---
+++ javascript-practice.js
@@ -0,0 +1,26 @@
+//Declare a string variable in javascriptlet {
+    let  name ="Yvonne";
+    console.log(name);
+
+    //convert a javascript string to Uppercase
+   let string="sad";
+   let string1=string.toUpperCase();
+   console.log(string1);
+    
+     
+
+//How to get the length of javascript string
+let text="my name is yvonne";
+let length =text.length;
+console.log(length);
+
+//convert a javascript string to lowercase
+let string2="HAPPY";
+let string3=string2.toLowerCase();
+console.log(string3);
+
+
+//convert a javascript to number
+let myString=("77.22");
+console.log("77.22"*100);
+;
